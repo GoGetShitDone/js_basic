bd34b38f91d1128304b03d368be4b73b87f3fca5
Ullala David <rkdrms2020@icloud.com>
2024-10-15 18:06:10 +0900

    JS Event Listener click, mouse enter, mouse leave, resize, copy, offline, onelin

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const search = document.querySelector(".hello h1");
 
 const searchAll = document.querySelectorAll(".hello h1");
 
+const searchDetail = document.querySelector(".hello:first-child h1");
+
+
 title.innerText = "YES!! GO! GET SHIT DONE!";
 
 console.log(title.id);
@@ -19,4 +22,78 @@ console.dir(title);
 console.dir(search);
 console.log(search);
 console.log(searchAll);
+console.log(searchDetail);
+console.log(title);
+console.dir(searchDetail);
+console.dir(title);
 
+title.style.color = "blue";
+
+///////////////////////////////
+//////////[- EVENT -]//////////
+///////////////////////////////
+
+
+// - Enter -
+
+function handleTitleClick() {
+    console.log("title was Clicked");
+    title.style.color = "pink";
+}
+
+
+// - Mouse Enter -
+
+function handleMouseEnter() {
+    console.log("Mouse is here");
+    title.style.color = "red";
+}
+
+
+// - Mouse Leave -
+
+function handleMouseLeave() {
+    console.log("Mouse leaved");
+    title.style.color = "blue";
+}
+
+
+// - Resize -
+
+function handleWindowResize() {
+    document.body.style.backgroundColor = "black";
+}
+
+// - copied -
+
+function handleWindowCopy() {
+    document.body.style.backgroundColor = "Red";
+    console.log("copied");
+}
+
+
+function handleWindowOffline() {
+    alert("SOS NO WIFI");
+}
+
+function handleWindowOnline() {
+    alert("YES! HERE IS WIFI!");
+}
+
+title.addEventListener("click", handleTitleClick);
+// title.onclick = handleTitleClick;
+
+title.addEventListener("mouseleave", handleMouseLeave);
+// title.onmouseleave = handleMouseLeave;
+
+title.addEventListener("mouseenter", handleMouseEnter);
+// title.onmouseenter = handleMouseEnter;
+
+window.addEventListener("resize", handleWindowResize);
+// window.onresize = handleWindowResize;
+
+window.addEventListener("copy", handleWindowCopy);
+// window.oncopy = handleWindowCopy;
+
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
