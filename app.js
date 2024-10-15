abf2b8971d4d8c48f876da08388178c56032ea7b
Ullala David <rkdrms2020@icloud.com>
2024-10-15 10:15:37 +0900

    Browser Connecting and Communicainge update

---
+++ app.js
@@ -0,0 +1,22 @@
+const woo = document.getElementsByClassName("WOOWOOO");
+
+const hello = document.getElementsByTagName("h1");
+
+const title = document.getElementById("title");
+
+const search = document.querySelector(".hello h1");
+
+const searchAll = document.querySelectorAll(".hello h1");
+
+title.innerText = "YES!! GO! GET SHIT DONE!";
+
+console.log(title.id);
+console.log(title.className);
+
+console.dir(woo);
+console.dir(hello);
+console.dir(title);
+console.dir(search);
+console.log(search);
+console.log(searchAll);
+
